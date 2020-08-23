2913acee737af10d922f0b8c8ff8f2b57c333ab3
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2020-08-23 02:29:27 -0300

    add command service to providers

--- packages/bot/src/app.module.ts
+++ packages/bot/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { BotService } from './bot.service';
 import { DatabaseModule } from './database/database.module';
+import { CommandService } from './command.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [AppController],
-  providers: [BotService],
+  providers: [BotService, CommandService],
 })
 export class AppModule { }
