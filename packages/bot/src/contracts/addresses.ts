941dafb96fae5d9f6d0a31b325e4fd5d0bffb8e3
thegostep <thegostep@gmail.com>
2020-08-24 20:21:19 +0000

    add kovan address

--- packages/bot/src/contracts/addresses.ts
+++ packages/bot/src/contracts/addresses.ts
@@ -5,6 +5,12 @@ const addresses = {
     DAI: "0x6B175474E89094C44Da98b954EedeAC495271d0F",
     trees: "0x358F6260F1f90cD11a10E251ce16EA526f131B02",
   },
+  kovan: {
+    rDAI: "0x462303f77a3f17Dbd95eb7bab412FE4937F9B9CB",
+    cDAI: "0xe7bc397DBd069fC7d0109C0636d06888bb50668c",
+    DAI: "0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa",
+    trees: "0x358F6260F1f90cD11a10E251ce16EA526f131B02",
+  },
 };
 
 export default addresses;
