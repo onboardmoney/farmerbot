d8eb843014c2663261a63ce3ac9025865c7b0573
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2020-08-24 15:48:32 -0300

    remove global decorator

--- packages/bot/src/database/database.module.ts
+++ packages/bot/src/database/database.module.ts
@@ -1,8 +1,7 @@
-import { Module, Global } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { RedisModule } from 'nestjs-redis'
 import { DatabaseService } from './database.service';
 
-@Global()
 @Module({
   imports: [RedisModule.register({
     url: 'redis://localhost:6379',
