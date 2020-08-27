fc47c3ce079d2c6d3b70bc6c4222d9a4c0617ace
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2020-08-27 17:11:50 -0300

    subgraph wip

--- packages/bot/src/subgraph.service.ts
+++ packages/bot/src/subgraph.service.ts
@@ -2,13 +2,17 @@ import { Injectable } from '@nestjs/common';
 import { DatabaseService } from './database/database.service';
 import axios, { AxiosInstance } from "axios";
 import { Cron } from '@nestjs/schedule';
+import { CommandService } from './command.service';
 
 @Injectable()
 export class SubGraphService {
 
   axiosInstance: AxiosInstance;
 
-  constructor(private readonly db: DatabaseService) {
+  constructor(
+    private readonly db: DatabaseService,
+    private readonly cmdService: CommandService
+  ) {
     const url = "https://api.thegraph.com"
     this.axiosInstance = axios.create({
       baseURL: url
@@ -18,13 +22,27 @@ export class SubGraphService {
   @Cron("*/15 * * * * *")
   async getTransfers() {
     const query = {
-      "query": "{tokens(first: 5) { id decimals name symbol } transfers(first: 5) { id from to value }}"
+      "query": `{
+        transfers(where: { to: "0x6B175474E89094C44Da98b954EedeAC495271d0F" } ) {
+          id
+          from
+          to
+          value
+        }
+      }`
     }
     console.log('pulling transfers')
     const ret = await this.axiosInstance.post(
       '/subgraphs/name/itirabasso/farmerbot',
       query
     )
-    // console.log('response:', ret)
+    const { data } = ret;
+    if (data === undefined) return;
+    console.log(data)
+    // const { transfers } = data;
+    // for (const transfer of transfers) {
+    //   await this.cmdService.doPlant(transfer.from)
+    // }
+    // console.log('response:', data)
   }
 }
