a54e0f7e0f9b431b65f1a2c627c3ce1762a2d772
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2020-09-12 12:05:15 -0300

    remove unused code

--- packages/bot/src/main.ts
+++ packages/bot/src/main.ts
@@ -8,13 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn', 'debug'],
   });
-  // TODO : remove this!
-  app.connectMicroservice({
-    transport: Transport.REDIS,
-    options: {
-      url: 'redis://localhost:6379',
-    }
-  })
   await app.listen(3000);
 }
 bootstrap();
