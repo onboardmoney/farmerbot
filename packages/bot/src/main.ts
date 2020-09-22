da84f1ef72c4dcd34878985389208471d132174c
Ignacio M. Tirabasso <ignacio.tirabasso@gmail.com>
2020-09-22 17:57:52 -0300

    enable https

--- packages/bot/src/main.ts
+++ packages/bot/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { readFileSync } from 'fs';
 
 declare const module: any
 
+require('dotenv').config()
+
 async function bootstrap() {
+ 
+  const keyFile = readFileSync(process.env.PRIVATE_KEY_SSL_FILEPATH || 'ssl/localhost.key.pem')
+  const certFile = readFileSync(process.env.CERTIFICATE_SSL_FILEPATH || 'ssl/localhost.crt.pem')
+
   const app = await NestFactory.create(AppModule, {
+    httpsOptions: {
+      key: keyFile,
+      cert: certFile,
+    },
     logger: ['error', 'warn', 'debug'],
   });
   await app.listen(3000);
